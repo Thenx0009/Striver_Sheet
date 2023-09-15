7664d0d911950315cf8870a8c535ce0b6a229bf6
Thenx0009 <ayushverm0009@gmail.com>
2023-09-15 16:10:36 +0530

    Initial commit

---
+++ Arrays/Majority_Elemet_N3.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Majority_Elemet_N3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {1,1,1,3,3,2,2,2};
+		System.out.println( majority(arr));
+	}
+	public static List<Integer> majority(int arr[]){
+		ArrayList<Integer> ll = new ArrayList<>();
+		
+		for(int i=0;i<arr.length;i++) {
+			if(ll.size()==0 || ll.get(0)!=arr[i]) {
+				int count=0;
+				for(int j=0;j<arr.length;j++) {
+					if(arr[j]==arr[i]) {
+						count++;
+					}
+				}
+				if(count>arr.length/3) {
+					ll.add(arr[i]);
+				}
+			}
+			if(ll.size()==2) {
+				break;
+			}
+		}
+		return ll;
+	}
+
+}
