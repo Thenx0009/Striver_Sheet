e4c4991d52e3d4482938f522c384085b10bcb1d9
Thenx0009 <ayushverm0009@gmail.com>
2023-10-16 19:56:10 +0530

    Initial Commit

--- Linked_List/Delete_Node_in_a_LinkedList.java
+++ Linked_List/Delete_Node_in_a_LinkedList.java
@@ -2,9 +2,36 @@ package Linked_List;
 
 public class Delete_Node_in_a_LinkedList {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class ListNode {
+		int val;
+		ListNode next;
 
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
 	}
 
+	class Solution {
+		public ListNode deleteMiddle(ListNode head) {
+			if(head==null || head.next==null)return null;
+			ListNode slow = head;
+			ListNode fast = head;
+			ListNode prevs = null;
+			while (fast != null && fast.next != null) {
+				prevs = slow;
+				slow = slow.next;
+				fast = fast.next.next;
+			}
+			prevs.next = slow.next;
+			return head;
+		}
+	}
 }
