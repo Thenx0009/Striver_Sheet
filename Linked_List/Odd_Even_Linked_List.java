d0006e2e8da9055ee0a07e1f9225cf0952bea2b8
Thenx0009 <ayushverm0009@gmail.com>
2023-10-17 12:29:15 +0530

    Initial Commit

--- Linked_List/Odd_Even_Linked_List.java
+++ Linked_List/Odd_Even_Linked_List.java
@@ -2,4 +2,44 @@ package Linked_List;
 
 public class Odd_Even_Linked_List {
 
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	class Solution {
+		public ListNode oddEvenList(ListNode head) {
+			if(head==null) {
+				return head;
+			}
+			ListNode odd = head;
+			ListNode even = head.next;
+			
+			//maintain the even head;
+			ListNode evenHead = head.next;
+			
+			while(even!=null && even.next!=null) {
+				//change pointers for odd list
+				odd.next = odd.next.next;
+				odd= odd.next;
+				//change pointers for even list
+				even.next = even.next.next;
+				even =  even.next;
+			}
+			odd.next = evenHead;
+			return head;
+		}
+	}
 }
